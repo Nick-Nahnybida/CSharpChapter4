adcab5877574a915058b2fff6f7be1299bbe9b2b
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-11-19 12:12:14 -0600

    update

---
+++ NumberPicker/NumberPicker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NumberPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            
+
+            WriteLine("Enter a number; 1, 2, or 3");
+            userInput = int.Parse(ReadLine());
+
+            if (userInput == 1 || userInput == 3)
+            {
+                WriteLine("You have lost");
+            }
+            else if (userInput == 2)
+            {
+                WriteLine("You win!");
+            }
+            else if (userInput >= 3)
+            {
+                WriteLine("Error");
+            }
+        }
+    }
+}
