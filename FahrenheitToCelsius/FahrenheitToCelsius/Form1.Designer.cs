edc2cbe0f268d5717e067c62e84bc582ceb8dfb3
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-11-20 13:01:33 -0600

    update

--- FahrenheitToCelsius/FahrenheitToCelsius/Form1.Designer.cs
+++ FahrenheitToCelsius/FahrenheitToCelsius/Form1.Designer.cs
@@ -28,18 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFahrenheitToCelsius));
             this.lbl8AM = new System.Windows.Forms.Label();
             this.lblNoon = new System.Windows.Forms.Label();
             this.lbl5PM = new System.Windows.Forms.Label();
             this.txt8AMInput = new System.Windows.Forms.TextBox();
             this.txtNoonInput = new System.Windows.Forms.TextBox();
             this.txt5PMInput = new System.Windows.Forms.TextBox();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblEnterFahrenheit = new System.Windows.Forms.Label();
+            this.btnFahrenheit = new System.Windows.Forms.Button();
             this.lbl8AMResult = new System.Windows.Forms.Label();
             this.lblNoonResult = new System.Windows.Forms.Label();
             this.lbl5PMResult = new System.Windows.Forms.Label();
-            this.lblCelsiusTitle = new System.Windows.Forms.Label();
+            this.lblCelsiusResult = new System.Windows.Forms.Label();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.picCelsius = new System.Windows.Forms.PictureBox();
+            this.picFahrenheit = new System.Windows.Forms.PictureBox();
+            this.lblEnterCelsius = new System.Windows.Forms.Label();
+            this.lblFahrenheitResult = new System.Windows.Forms.Label();
+            this.lbl5PM2 = new System.Windows.Forms.Label();
+            this.lblNoon2 = new System.Windows.Forms.Label();
+            this.lbl8AM2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCelsius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFahrenheit)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl8AM
@@ -90,68 +101,152 @@
             this.txt5PMInput.Size = new System.Drawing.Size(100, 20);
             this.txt5PMInput.TabIndex = 5;
             // 
-            // lblTitle
+            // lblEnterFahrenheit
             // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(23, 25);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(282, 13);
-            this.lblTitle.TabIndex = 6;
-            this.lblTitle.Text = "Please enter the temps in Fahrenheit for these times of day";
+            this.lblEnterFahrenheit.AutoSize = true;
+            this.lblEnterFahrenheit.Location = new System.Drawing.Point(23, 25);
+            this.lblEnterFahrenheit.Name = "lblEnterFahrenheit";
+            this.lblEnterFahrenheit.Size = new System.Drawing.Size(215, 13);
+            this.lblEnterFahrenheit.TabIndex = 6;
+            this.lblEnterFahrenheit.Text = "Temperatures During The Day In Fahrenheit";
             // 
-            // btnCalculate
+            // btnFahrenheit
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(330, 119);
-            this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btnCalculate.TabIndex = 7;
-            this.btnCalculate.Text = "Calculate";
-            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnFahrenheit.Location = new System.Drawing.Point(321, 137);
+            this.btnFahrenheit.Name = "btnFahrenheit";
+            this.btnFahrenheit.Size = new System.Drawing.Size(75, 23);
+            this.btnFahrenheit.TabIndex = 7;
+            this.btnFahrenheit.Text = "Fahrenheit";
+            this.btnFahrenheit.UseVisualStyleBackColor = true;
+            this.btnFahrenheit.Click += new System.EventHandler(this.btnFahrenheit_Click);
             // 
             // lbl8AMResult
             // 
             this.lbl8AMResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lbl8AMResult.Location = new System.Drawing.Point(426, 68);
+            this.lbl8AMResult.Location = new System.Drawing.Point(601, 68);
             this.lbl8AMResult.Name = "lbl8AMResult";
-            this.lbl8AMResult.Size = new System.Drawing.Size(100, 23);
+            this.lbl8AMResult.Size = new System.Drawing.Size(187, 23);
             this.lbl8AMResult.TabIndex = 8;
             // 
             // lblNoonResult
             // 
             this.lblNoonResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblNoonResult.Location = new System.Drawing.Point(426, 119);
+            this.lblNoonResult.Location = new System.Drawing.Point(601, 119);
             this.lblNoonResult.Name = "lblNoonResult";
-            this.lblNoonResult.Size = new System.Drawing.Size(100, 23);
+            this.lblNoonResult.Size = new System.Drawing.Size(187, 23);
             this.lblNoonResult.TabIndex = 9;
             // 
             // lbl5PMResult
             // 
             this.lbl5PMResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lbl5PMResult.Location = new System.Drawing.Point(426, 165);
+            this.lbl5PMResult.Location = new System.Drawing.Point(601, 165);
             this.lbl5PMResult.Name = "lbl5PMResult";
-            this.lbl5PMResult.Size = new System.Drawing.Size(100, 23);
+            this.lbl5PMResult.Size = new System.Drawing.Size(187, 23);
             this.lbl5PMResult.TabIndex = 10;
             // 
-            // lblCelsiusTitle
+            // lblCelsiusResult
             // 
-            this.lblCelsiusTitle.AutoSize = true;
-            this.lblCelsiusTitle.Location = new System.Drawing.Point(426, 25);
-            this.lblCelsiusTitle.Name = "lblCelsiusTitle";
-            this.lblCelsiusTitle.Size = new System.Drawing.Size(106, 13);
-            this.lblCelsiusTitle.TabIndex = 11;
-            this.lblCelsiusTitle.Text = "The temps in celsius:";
+            this.lblCelsiusResult.AutoSize = true;
+            this.lblCelsiusResult.Location = new System.Drawing.Point(534, 25);
+            this.lblCelsiusResult.Name = "lblCelsiusResult";
+            this.lblCelsiusResult.Size = new System.Drawing.Size(120, 13);
+            this.lblCelsiusResult.TabIndex = 11;
+            this.lblCelsiusResult.Text = "Temperatures In Celsius";
+            // 
+            // btnCelsius
+            // 
+            this.btnCelsius.Location = new System.Drawing.Point(417, 137);
+            this.btnCelsius.Name = "btnCelsius";
+            this.btnCelsius.Size = new System.Drawing.Size(75, 23);
+            this.btnCelsius.TabIndex = 12;
+            this.btnCelsius.Text = "Celsius";
+            this.btnCelsius.UseVisualStyleBackColor = true;
+            this.btnCelsius.Click += new System.EventHandler(this.btnCelsius_Click);
+            // 
+            // picCelsius
+            // 
+            this.picCelsius.Image = ((System.Drawing.Image)(resources.GetObject("picCelsius.Image")));
+            this.picCelsius.Location = new System.Drawing.Point(321, 25);
+            this.picCelsius.Name = "picCelsius";
+            this.picCelsius.Size = new System.Drawing.Size(171, 106);
+            this.picCelsius.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCelsius.TabIndex = 13;
+            this.picCelsius.TabStop = false;
+            // 
+            // picFahrenheit
+            // 
+            this.picFahrenheit.Image = ((System.Drawing.Image)(resources.GetObject("picFahrenheit.Image")));
+            this.picFahrenheit.Location = new System.Drawing.Point(321, 25);
+            this.picFahrenheit.Name = "picFahrenheit";
+            this.picFahrenheit.Size = new System.Drawing.Size(171, 106);
+            this.picFahrenheit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFahrenheit.TabIndex = 14;
+            this.picFahrenheit.TabStop = false;
+            // 
+            // lblEnterCelsius
+            // 
+            this.lblEnterCelsius.AutoSize = true;
+            this.lblEnterCelsius.Location = new System.Drawing.Point(23, 25);
+            this.lblEnterCelsius.Name = "lblEnterCelsius";
+            this.lblEnterCelsius.Size = new System.Drawing.Size(198, 13);
+            this.lblEnterCelsius.TabIndex = 15;
+            this.lblEnterCelsius.Text = "Temperatures During The Day In Celsius";
+            // 
+            // lblFahrenheitResult
+            // 
+            this.lblFahrenheitResult.AutoSize = true;
+            this.lblFahrenheitResult.Location = new System.Drawing.Point(531, 25);
+            this.lblFahrenheitResult.Name = "lblFahrenheitResult";
+            this.lblFahrenheitResult.Size = new System.Drawing.Size(137, 13);
+            this.lblFahrenheitResult.TabIndex = 16;
+            this.lblFahrenheitResult.Text = "Temperatures In Fahrenheit";
+            // 
+            // lbl5PM2
+            // 
+            this.lbl5PM2.AutoSize = true;
+            this.lbl5PM2.Location = new System.Drawing.Point(537, 172);
+            this.lbl5PM2.Name = "lbl5PM2";
+            this.lbl5PM2.Size = new System.Drawing.Size(50, 13);
+            this.lbl5PM2.TabIndex = 19;
+            this.lbl5PM2.Text = "5:00 PM:";
+            // 
+            // lblNoon2
+            // 
+            this.lblNoon2.AutoSize = true;
+            this.lblNoon2.Location = new System.Drawing.Point(551, 126);
+            this.lblNoon2.Name = "lblNoon2";
+            this.lblNoon2.Size = new System.Drawing.Size(36, 13);
+            this.lblNoon2.TabIndex = 18;
+            this.lblNoon2.Text = "Noon:";
+            // 
+            // lbl8AM2
+            // 
+            this.lbl8AM2.AutoSize = true;
+            this.lbl8AM2.Location = new System.Drawing.Point(534, 73);
+            this.lbl8AM2.Name = "lbl8AM2";
+            this.lbl8AM2.Size = new System.Drawing.Size(50, 13);
+            this.lbl8AM2.TabIndex = 17;
+            this.lbl8AM2.Text = "8:00 AM:";
             // 
             // frmFahrenheitToCelsius
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblCelsiusTitle);
+            this.ClientSize = new System.Drawing.Size(800, 225);
+            this.Controls.Add(this.lbl5PM2);
+            this.Controls.Add(this.lblNoon2);
+            this.Controls.Add(this.lbl8AM2);
+            this.Controls.Add(this.lblFahrenheitResult);
+            this.Controls.Add(this.lblEnterCelsius);
+            this.Controls.Add(this.picFahrenheit);
+            this.Controls.Add(this.picCelsius);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.lblCelsiusResult);
             this.Controls.Add(this.lbl5PMResult);
             this.Controls.Add(this.lblNoonResult);
             this.Controls.Add(this.lbl8AMResult);
-            this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnFahrenheit);
+            this.Controls.Add(this.lblEnterFahrenheit);
             this.Controls.Add(this.txt5PMInput);
             this.Controls.Add(this.txtNoonInput);
             this.Controls.Add(this.txt8AMInput);
@@ -160,6 +255,8 @@
             this.Controls.Add(this.lbl8AM);
             this.Name = "frmFahrenheitToCelsius";
             this.Text = "FahrenheitToCelsius";
+            ((System.ComponentModel.ISupportInitialize)(this.picCelsius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFahrenheit)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,12 +270,20 @@
         private System.Windows.Forms.TextBox txt8AMInput;
         private System.Windows.Forms.TextBox txtNoonInput;
         private System.Windows.Forms.TextBox txt5PMInput;
-        private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblEnterFahrenheit;
+        private System.Windows.Forms.Button btnFahrenheit;
         private System.Windows.Forms.Label lbl8AMResult;
         private System.Windows.Forms.Label lblNoonResult;
         private System.Windows.Forms.Label lbl5PMResult;
-        private System.Windows.Forms.Label lblCelsiusTitle;
+        private System.Windows.Forms.Label lblCelsiusResult;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.PictureBox picCelsius;
+        private System.Windows.Forms.PictureBox picFahrenheit;
+        private System.Windows.Forms.Label lblEnterCelsius;
+        private System.Windows.Forms.Label lblFahrenheitResult;
+        private System.Windows.Forms.Label lbl5PM2;
+        private System.Windows.Forms.Label lblNoon2;
+        private System.Windows.Forms.Label lbl8AM2;
     }
 }
 
--- FahrenheitToCelsius/FahrenheitToCelsius/Form1.cs
+++ FahrenheitToCelsius/FahrenheitToCelsius/Form1.cs
@@ -16,5 +16,133 @@ namespace FahrenheitToCelsius
         {
             InitializeComponent();
         }
+        private double temp8AM;
+        private double tempNoon;
+        private double temp5PM;
+        private double result8AM;
+        private double resultNoon;
+        private double result5PM;
+        private void btnFahrenheit_Click(object sender, EventArgs e)
+        {
+            picCelsius.Visible = false;
+            picFahrenheit.Visible = true;
+            lblEnterCelsius.Visible = false;
+            lblEnterFahrenheit.Visible = true;
+            lblCelsiusResult.Visible = false;
+            lblFahrenheitResult.Visible = true;
+
+            temp8AM = Convert.ToDouble(txt8AMInput.Text);
+            tempNoon = Convert.ToDouble(txtNoonInput.Text);
+            temp5PM = Convert.ToDouble(txt5PMInput.Text);
+
+            result8AM = (temp8AM * 1.8) + 32;
+            resultNoon = (tempNoon * 1.8) + 32;
+            result5PM = (temp5PM * 1.8) + 32;
+
+            if (result8AM <= 32)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + ", It is freezing outside";
+            }
+            else if (result8AM >= 32 && result8AM <= 100)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + "";
+            }
+            else if (result8AM > 100)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + ", It is hot outside";
+            }
+
+
+
+            if (resultNoon <= 32)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + ", It is freezing outside";
+            }
+            else if (resultNoon >= 32 && resultNoon <= 100)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + "";
+            }
+            else if (resultNoon > 100)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + ", It is hot outside";
+            }
+
+
+
+            if (result5PM <= 32)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + ", It is freezing outside";
+            }
+            else if (result5PM >= 32 && result5PM <= 100)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + "";
+            }
+            else if (result5PM > 100)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + ", It is hot outside";
+            }
+        }
+
+        private void btnCelsius_Click(object sender, EventArgs e)
+        {
+            picCelsius.Visible = true;
+            picFahrenheit.Visible = false;
+            lblEnterCelsius.Visible = true;
+            lblEnterFahrenheit.Visible = false;
+            lblCelsiusResult.Visible = false;
+            lblFahrenheitResult.Visible = true;
+
+            temp8AM = Convert.ToDouble(txt8AMInput.Text);
+            tempNoon = Convert.ToDouble(txtNoonInput.Text);
+            temp5PM = Convert.ToDouble(txt5PMInput.Text);
+
+            result8AM = (temp8AM - 32) / 1.8;
+            resultNoon = (tempNoon - 32) / 1.8;
+            result5PM = (temp5PM - 32) / 1.8;
+
+            
+            if (result8AM <= 32)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + ", It is freezing outside";
+            }
+            else if (result8AM >= 32 && result8AM <= 100)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + "";
+            }
+            else if (result8AM > 100)
+            {
+                lbl8AMResult.Text = Math.Round(result8AM, 2) + ", It is hot outside";
+            }
+
+
+
+            if (resultNoon <= 32)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + ", It is freezing outside";
+            }
+            else if (resultNoon >= 32 && resultNoon <= 100)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + "";
+            }
+            else if (resultNoon > 100)
+            {
+                lblNoonResult.Text = Math.Round(resultNoon, 2) + ", It is hot outside";
+            }
+
+
+
+            if (result5PM <= 32)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + ", It is freezing outside";
+            }
+            else if (result5PM >= 32 && result5PM <= 100)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + "";
+            }
+            else if (result5PM > 100)
+            {
+                lbl5PMResult.Text = Math.Round(result5PM, 2) + ", It is hot outside";
+            }
+        }
     }
 }
